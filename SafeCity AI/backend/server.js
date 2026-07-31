5fcb8dbdbe2a8390b2bbc11786e23520c74e211e
Harsh Varma <hv87805@gmail.com>
2026-07-31 11:20:34 +0530

    Update server.js

--- SafeCity AI/backend/server.js
+++ SafeCity AI/backend/server.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 // Serve frontend
-app.use(express.static(path.join(__dirname, "frontend")));
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Prediction API
